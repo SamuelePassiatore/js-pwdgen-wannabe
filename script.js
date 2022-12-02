920aa2d878ab6e0b642afbca35effe46197b4c92
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-02 16:10:57 +0100

    added span element to  index and analys in script

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log('JS OK');
+
+/* Chiedi all’utente il suo nome, 
+   poi chiedi il suo cognome, 
+   poi chiedi il suo colore preferito.
+   Infine scrivi sulla pagina nomecognomecolorepreferito21 */
+
+/* 
+1) Prendo l'elemento da DOM
+2) Preparo una variabile con 21 come stringa
+3) Chiedo il nome all'utente e lo salvo in una variabile
+4) Chiedo il cognome all'utente e lo salvo in una variabile
+5) Chiedo il colore preferito all'utente e lo salvo in una variabile
+6) Sommo tutte le variabili come stringa
+7) Inserisco il risultato nell'elemento del DOM
+ */
+
+
+
+
+
