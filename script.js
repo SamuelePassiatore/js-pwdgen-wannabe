8e3d329b5c6929308bd9164740c25918d4365c50
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-02 17:26:28 +0100

    variable name, variable surname, variable color

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ console.log('JS OK');
 /* 
 1) Prendo l'elemento da DOM
 2) Preparo una variabile con 21 come stringa
-3) Chiedo il nome all'utente e lo salvo in una variabile
-4) Chiedo il cognome all'utente e lo salvo in una variabile
-5) Chiedo il colore preferito all'utente e lo salvo in una variabile
+3) Chiedo il nome all'utente 
+4) Chiedo il cognome all'utente 
+5) Chiedo il colore preferito all'utente 
 6) Sommo tutte le variabili come stringa
 7) Inserisco il risultato nell'elemento del DOM
  */
@@ -23,6 +23,23 @@ const passwordElement = document.getElementById('password');
 
 // Preparo una variabile con 21 come stringa
 const finalNumber = 21;
+////console.log(finalNumber);
+
+// Chiedo il nome all'utente
+const userName = prompt('Qual è il tuo nome?', 'Napoleone');
+////console.log(userName);
+
+// Chiedo il cognome all'utente
+const userSurname = prompt('Qual è il tuo cognome?', 'Bonaparte');
+////console.log(userSurname);
+
+// Chiedo all'utente il suo colore preferito
+const favColor = prompt('Qual è il tuo colore preferito?', 'Verde');
+////console.log(favColor);
+
+
+
+
 
 
 
